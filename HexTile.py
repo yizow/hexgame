babd665d6567463bf2369d742608e51ac619819a
Yizhe Liu <yizow@berkeley.edu>
2020-05-27 15:28:28 -0700

    use property decorator

--- HexTile.py
+++ HexTile.py
@@ -9,10 +9,16 @@ import numpy as np
 class HexTile:
   """Represents a single tile"""
 
-  def __init__(self, *center):
-    self.set_center(*center);
+  def __init__(self, radius, *center):
+    self.radius = radius
+    self.center = center
 
-  def set_center(self, *center):
+  @property
+  def center(self):
+    return (self.x, self.y, self.z)
+
+  @center.setter
+  def center(self, center):
     num_args = len(center)
     if num_args == 2:
       self.x, self.y = center
@@ -22,15 +28,16 @@ class HexTile:
     else:
       raise TypeError("Wrong number of arugment for HexTile center")
 
-  def get_corners(self, radius):
-    return [self.calc_corner(radius, degree) for degree in range(0, 360, 60)]
+  @property
+  def corners(self):
+    return [self.calc_corner(self.radius, degree) for degree in range(0, 360, 60)]
 
   def calc_corner(self, radius, angle):
     return (self.x + radius * math.cos(math.radians(angle)), self.y + radius * math.sin(math.radians(angle)))
 
 
 class HexMap:
-  def __init__(self, width=25, height=10, tile_radius=50):
+  def __init__(self, width=15, height=9, tile_radius=50):
     self.width = width
     self.height = height
     self.tile_radius = tile_radius
@@ -39,11 +46,11 @@ class HexMap:
     for x in range(width):
       row = []
       for y in range(height):
-        center_x = x * tile_radius * 1.5
-        center_y = y * tile_radius * math.sqrt(3)
+        center_x = x * self.tile_radius * 1.5
+        center_y = y * self.tile_radius * math.sqrt(3)
         if x % 2:
-          center_y += tile_radius * math.sqrt(3) * .5
-        row.append(HexTile(center_x, center_y))
+          center_y += self.tile_radius * math.sqrt(3) * .5
+        row.append(HexTile(self.tile_radius, 2 * self.tile_radius + center_x, 2 * self.tile_radius + center_y))
       self.map.append(row)
 
   def __getitem__(self, pos):
@@ -63,11 +70,21 @@ class HexMap:
         yield self[x, y]
 
 
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 900
+
+COLOR = (255, 0, 0)
+RADIUS = 50
+BORDER_BUFFER = 2
+
 def main():
   pygame.display.quit()
   pygame.display.init()
-  main_display = pygame.display.set_mode(size=(1600,900), flags=pygame.RESIZABLE)
-  for tile in HexMap():
-    pygame.draw.polygon(main_display, (255, 0, 0), tile.get_corners(50), 1)
+
+  main_display = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT), flags=pygame.RESIZABLE)
+  num_tiles_width = int((SCREEN_WIDTH - BORDER_BUFFER * RADIUS) / (1.5 * RADIUS))
+  num_tiles_height = int((SCREEN_HEIGHT - BORDER_BUFFER * RADIUS) / (math.sqrt(3) * RADIUS))
+  for tile in HexMap(num_tiles_width, num_tiles_height, RADIUS):
+    pygame.draw.polygon(main_display, COLOR, tile.corners, 1)
 
   pygame.display.update()
