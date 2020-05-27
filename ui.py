fb1587522913b1632aea92cb2c56fdaf9bb6a3a6
Yizhe Liu <yizow@berkeley.edu>
2020-05-27 15:55:48 -0700

    rename hexmap

--- ui.py
+++ ui.py
@@ -3,7 +3,7 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 
-import HexTile
+import HexMap
 
 SCREEN_WIDTH = 1600
 SCREEN_HEIGHT = 900
@@ -17,9 +17,9 @@ def main():
   pygame.display.init()
 
   main_display = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT), flags=pygame.RESIZABLE)
-  num_tiles_width = int((SCREEN_WIDTH - BORDER_BUFFER * RADIUS) / HexTile.HexMap.calc_tile_spacing_horiz(RADIUS))
-  num_tiles_height = int((SCREEN_HEIGHT - BORDER_BUFFER * RADIUS) / HexTile.HexMap.calc_tile_spacing_vert(RADIUS))
-  for tile in HexTile.HexMap(num_tiles_width, num_tiles_height, RADIUS):
+  num_tiles_width = int((SCREEN_WIDTH - BORDER_BUFFER * RADIUS) / HexMap.HexMap.calc_tile_spacing_horiz(RADIUS))
+  num_tiles_height = int((SCREEN_HEIGHT - BORDER_BUFFER * RADIUS) / HexMap.HexMap.calc_tile_spacing_vert(RADIUS))
+  for tile in HexMap.HexMap(num_tiles_width, num_tiles_height, RADIUS):
     pygame.draw.polygon(main_display, COLOR, tile.corners, 1)
 
   pygame.display.update()
