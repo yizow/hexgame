dbc98092f931debdcb780b54c59a1cb08a5dcbd2
Yizhe Liu <yizow@berkeley.edu>
2020-05-28 23:49:49 -0700

    attempt to get graphical grid working

--- HexMap.py
+++ HexMap.py
@@ -14,16 +14,13 @@ class HexTile:
 
     @property
     def center(self):
-        return (self.x, self.y, self.z)
+        return (self.q, self.r)
 
     @center.setter
     def center(self, center):
         num_args = len(center)
         if num_args == 2:
-            self.x, self.y = center
-            self.z = 0 - self.x - self.y
-        elif num_args == 3:
-            self.x, self.y, self.z = center
+            self.q, self.r = center
         else:
             raise TypeError("Wrong number of arugment for HexTile center")
 
@@ -32,14 +29,17 @@ class HexTile:
         return [self.calc_corner(self.radius, degree) for degree in range(0, 360, 60)]
 
     def calc_corner(self, radius, angle):
-        return tuple(map(round, (self.x + radius * math.cos(math.radians(angle)), self.y + radius * math.sin(math.radians(angle)))))
+        return tuple(map(round, (self.q + radius * math.cos(math.radians(angle)), self.r + radius * math.sin(math.radians(angle)))))
 
 
 class HexMap:
+    directions = [(1, -1), (1, 0), (0, 1), (-1, 1), (-1, 0), (0, -1)]
+
     def __init__(self, width=15, height=9, tile_radius=50):
         self.width = width
         self.height = height
         self.tile_radius = tile_radius
+        self.border_offset = 1 * self.tile_radius
 
         self.tile_spacing_horiz = type(
             self).calc_tile_spacing_horiz(self.tile_radius)
@@ -47,37 +47,67 @@ class HexMap:
             self).calc_tile_spacing_vert(self.tile_radius)
 
         self.map = []
-        for x in range(width):
+        for q in range(width):
             row = []
-            for y in range(height):
-                center_x = x * self.tile_spacing_horiz
-                center_y = y * self.tile_spacing_vert
-                if x % 2:
-                    center_y += self.tile_spacing_vert * .5
+            for r in range(height):
+                center_q = q * self.tile_spacing_horiz
+                center_r = r * self.tile_spacing_vert
+                if q % 2:
+                    center_r += self.tile_spacing_vert * .5
                 # negative coordinates get cut off
-                border_offset = 1 * self.tile_radius
-                row.append(HexTile(self.tile_radius, border_offset +
-                                   center_x, border_offset + center_y))
+                row.append(HexTile(self.tile_radius, self.border_offset +
+                                   center_q, self.border_offset + center_r))
             self.map.append(row)
 
     def __getitem__(self, pos):
-        x, y = pos
-        return self.map[x][y]
+        q, r = pos
+        return self.map[q][r]
 
     def __setitem__(self, pos, tile):
-        x, y = pos
-        self.map[x][y] = tile
+        q, r = pos
+        self.map[q][r] = tile
 
     def __delitem__(self, pos):
         raise TypeError("Cannot delete tile from map")
 
     def __iter__(self):
-        for x in range(self.width):
-            for y in range(self.height):
-                yield self[x, y]
+        for q in range(self.width):
+            for r in range(self.height):
+                yield self[q, r]
 
     def calc_tile_spacing_horiz(radius):
         return radius * 1.5
 
     def calc_tile_spacing_vert(radius):
         return radius * math.sqrt(3)
+
+    def get_neighbors(self, q, r, distance=1):
+        neighbors = []
+        print("get_neighbors", [(distance * q, distance * r) for q, r in self.directions])
+        for q_delta, r_delta in [(distance * q, distance * r) for q, r in self.directions]:
+            neighbors.append(self[(q + q_delta) %
+                                  self.width, (r + r_delta) % self.height])
+        return neighbors
+
+    def hovered_tile(self, mouse_pos):
+        mouse_x, mouse_y = map(lambda x: x + self.border_offset, mouse_pos)
+        q = 2. / 3 * mouse_x / self.tile_radius
+        r = (-1. / 3 * mouse_x + math.sqrt(3) / 3 * mouse_y) / self.tile_radius
+        return type(self).hex_round(q, r)
+
+    def hex_round(q, r):
+        x, y, z = q, r, -q - r
+
+        rounded_x, rounded_y, rounded_z = map(round, (x, y, z))
+
+        x_delta, y_delta, z_delta = map(
+            abs, (x - rounded_x, y - rounded_y, z - rounded_z))
+
+        if x_delta > y_delta and x_delta > z_delta:
+            rounded_x = -rounded_y - rounded_z
+        elif y_delta > z_delta:
+            rounded_y = -rounded_x - rounded_z
+        else:
+            rounded_z = -rounded_x - rounded_y
+
+        return rounded_x, rounded_y
--- ui.py
+++ ui.py
@@ -2,19 +2,34 @@ import os
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
+from pygame.locals import *
 
+import constants
 import HexMap
 
 SCREEN_WIDTH = 1600
 SCREEN_HEIGHT = 900
 
-COLOR = (255, 0, 0)
 RADIUS = 50
 TILE_BUFFER = 1
 BUFFER = 50
 
+display = None
+
+
+def highlight_tile(tile):
+    return pygame.draw.polygon(display, WHITE, tile.corners, 1)
+
+
+def highlight_neighbors(hexmap, q, r):
+    print("highlight_neighbors", q, r)
+    neighbors = [highlight_tile(neighbor)
+                 for neighbor in hexmap.get_neighbors(q, r, distance=2)]
+    return neighbors[0].unionall(neighbors)
+
 
 def main():
+    global display
     pygame.display.quit()
     pygame.display.init()
 
@@ -32,10 +47,31 @@ def main():
     num_tiles_height = int((screen_height - TILE_BUFFER * RADIUS) /
                            HexMap.HexMap.calc_tile_spacing_vert(RADIUS))
 
-    for tile in HexMap.HexMap(num_tiles_width, num_tiles_height, RADIUS):
-        pygame.draw.polygon(display, COLOR, tile.corners, 1)
+    hexmap = HexMap.HexMap(num_tiles_width, num_tiles_height, RADIUS)
+    for tile in hexmap:
+        pygame.draw.polygon(display, RED, tile.corners, 1)
 
     if display == grid:
         main_display.blit(grid, (BUFFER, BUFFER))
 
     pygame.display.update()
+
+    running = True
+    while running:
+        event = pygame.event.wait()
+
+        if event.type == QUIT:
+            running = False
+            break
+
+        if event.type == MOUSEMOTION:
+            mouse_pos = pygame.mouse.get_pos()
+
+            grid_mouse_pos = (mouse_pos[0] - BUFFER, mouse_pos[1] - BUFFER)
+            hovered_tile = hexmap.hovered_tile(grid_mouse_pos)
+            updated_rect = highlight_neighbors(hexmap, *hovered_tile)
+            #main_display.blit(grid, updated_rect)
+
+        #pygame.display.update()
+
+    pygame.quit()
